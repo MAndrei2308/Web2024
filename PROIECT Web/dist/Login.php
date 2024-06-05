99e7057bedc7eb670572c178d4176a6f6ec2e254
MAndrei2308 <andrei.moisa.23@gmail.com>
2024-06-05 12:42:12 +0300

    ProfilePage + Mesaje_Login_Register

---
+++ PROIECT Web/dist/Login.php
@@ -0,0 +1,68 @@
+<?php
+session_start();
+
+if (isset($_SESSION['user_id'])) {
+    header("Location: Conectat_Home.php");
+    exit();
+}
+
+$error_message = isset($_SESSION['error_message']) ? $_SESSION['error_message'] : '';
+unset($_SESSION['error_message']);
+
+$success_message = isset($_SESSION['success_message']) ? $_SESSION['success_message'] : '';
+unset($_SESSION['success_message']);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="./css/Login.css">
+</head>
+
+
+<body>
+    <div class="login-container">
+        <h2>Login Form</h2>
+
+        <div class="pozaProfil">
+            <img src="../img/img_profil.png" alt="profil">
+        </div>
+        <form action="php/login.php" method="post">
+            <label for="username"><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="username" id="username" required>
+
+            <label for="password"><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="password" id="password" required>
+
+            <div class="check">
+                <input type="checkbox" name="rememberMe" id="rememberMe">
+                <label for="rememberMe">Remember me</label>
+            </div>
+
+            <button type="submit" class="submit">Login</button>
+            <div class="cancel-forgot">
+                <a href="PaginaPrincipala.php" class="cancelButton">Cancel</a>
+
+                <span class="password">Forgot <a href="#">password</a>? Or <a href="SignUp.php">sign up</a></span>
+            </div>
+        </form>
+
+        <?php if($error_message): ?>
+            <div class="error-message" style="color: red;">
+                <?php echo htmlspecialchars($error_message); ?>
+            </div>
+        <?php endif; ?>
+
+        <?php if($success_message): ?>
+            <div class="success_message" style="color: green;">
+                <?php echo htmlspecialchars($success_message); ?>
+            </div>
+        <?php endif; ?>
+
+    </div>
+</body>
+
+</html>
---
+++ PROIECT Web/dist/ProfilPagina.php
@@ -0,0 +1,116 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: Login.php");
+    exit();
+}
+
+include 'php/database.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$user_id = $_SESSION['user_id'];
+
+$query = "SELECT * FROM Profiles WHERE user_id = :user_id";
+$stmt = $db->prepare($query);
+$stmt->bindParam(':user_id', $user_id);
+$stmt->execute();
+
+$profile = $stmt->fetch(PDO::FETCH_ASSOC);
+
+$first_name = $profile['first_name'] ?? '';
+$last_name = $profile['last_name'] ?? '';
+$username = $profile['username'] ?? '';
+$gender = $profile['gender'] ?? '';
+$email = $profile['email'] ?? '';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/ProfilPaginaDesign.css">
+    <title>Profile</title>
+</head>
+
+<body>
+
+    <div class="profile-container">
+
+        <div class="profile-container__leftbox">
+            <nav>
+                <a href="php/profil.php" class="nav__user">
+                    <img src="../img/profile-user.png" alt="Profile" class="nav__button-user">
+                </a>
+                <a href="php/favorite.php">
+                    <img src="../img/iconheart-rose.png" alt="Favorite" class="nav__button-favorite">
+                </a>
+                <a href="php/logout.php">
+                    <img src="../img/logout.png" alt="Logout" class="nav__button-logout">
+                </a>
+                <a href="php/home.php">
+                    <img src="../img/undo.png" alt="Undo" class="nav__button-undo">
+                </a>
+            </nav>
+        </div>
+
+
+        <div class="profile-container__rightbox">
+            <div class="profile">
+                <h1 class="profile__title">Personal Info</h1>
+                <h2 class="profile__subtitle">Full Name</h2>
+                <form action="php/update.php" method="put">
+                    <p class="profile__info">
+                        <label for="first_name"></label>
+                        <input type="text" placeholder="<?php echo htmlspecialchars($first_name); ?>" name="first_name" id="first_name">
+                        
+                        <label for="last_name"></label>
+                        <input type="text" placeholder="<?php echo htmlspecialchars($last_name); ?>" name="last_name" id="last_name">
+
+                        <button class="profile__btn">update</button>
+                    </p>
+                </form>
+                <h2 class="profile__subtitle">Username</h2>
+                <form action="php/update.php" method="put">
+                    <p class="profile__info">
+                        <label for="username"></label>
+                        <input type="text" placeholder="<?php echo htmlspecialchars($username); ?>" name="username" id="username">
+
+                        <button class="profile__btn">update</button>
+                    </p>
+                </form>
+                <h2 class="profile__subtitle">Gender</h2>
+                <form action="php/update.php" method="put">
+                    <p class="profile__info">
+                        <label for="gender"></label>
+                        <input type="text" placeholder="<?php echo htmlspecialchars($gender); ?>" name="gender" id="gender">
+
+                        <button class="profile__btn">update</button>
+                    </p>
+                </form>
+                <h2 class="profile__subtitle">Email</h2>
+                <form action="php/update.php" method="put">
+                    <p class="profile__info">
+                        <label for="email"></label>
+                        <input type="text" placeholder="<?php echo htmlspecialchars($email); ?>" name="email" id="email">
+
+                        <button class="profile__btn">update</button>
+                    </p>
+                </form>
+                <h2 class="profile__subtitle">Password</h2>
+                <p class="profile__info">
+                    <a href="ChangePassword.php">Change password</a>
+                </p>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
